a3eb47f6601f849c45e229eb46b57e7e8f7860ad
jvc-eng <70835122+jvc-eng@users.noreply.github.com>
2025-11-22 09:52:34 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: 'app.js',
+    filename: 'app.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
@@ -12,12 +13,46 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'functions',
+          to: 'functions',
+          noErrorOnMissing: true
+        }
+      ],
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   devServer: {
     static: './dist',
     port: 3000,
     open: true,
   },
-  mode: 'production',
 };
