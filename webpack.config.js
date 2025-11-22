d5415a65df399078042d93d2d988d427433821ca
jvc-eng <70835122+jvc-eng@users.noreply.github.com>
2025-11-22 09:16:05 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    port: 3000,
+    open: true,
+  },
+  mode: 'production',
+};
